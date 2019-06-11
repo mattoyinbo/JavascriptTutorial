4e6bb0f2482e8b1c028bbfa23229f813df9feefd
mattoyinbo <mattoyinbo@gmail,com>
2019-06-10 20:15:28 -0400

    Update image files

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let heading = document.getElementById('header');
 heading.style.backgroundColor = 'red';
 
 let btn = document.getElementById('btn');
-btn.style.backgroundColor = 'red';
+btn.style.backgroundImage = "url('images.jpg')";
+btn.style.color = 'red';
 console.log(heading);
 console.log(btn);
